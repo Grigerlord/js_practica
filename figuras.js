a7a3fabbe039a97709439b1228085e6197eb7c16
Griger <grigerratia@gmail.com>
2021-12-25 22:29:14 -0400

    Agregué HTML y JS

---
+++ figuras.js
@@ -0,0 +1,19 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado mide: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado mide: " + areaCuadrado + "cm cuadrados");
+
+// Código del triágulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 4;
+
+console.log("Los lados del triángulo miden: "
+    + ladoTriangulo1 + "cm, "
+    + ladoTriangulo2 + "cm, "
+    + ladoTriangulo3 + "cm.")
